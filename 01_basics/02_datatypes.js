d4dd95f99e12b7bd8747e1d2eaf0a3a3e9b77598
mgcoder27 <mgcoder2027@gmail.com>
2025-01-18 19:19:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+let name = "Manu";
+let age = 30;
+let isLoggedIn = true;
+let state; // undefined datatype type is decided but value is not
+
+// number=> 2 to the power of 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined
+// symbol=>uniqueness
+
+// object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+
