89a0ebc273f7b26b2267902685f144d4414e5dd6
mgcoder27 <mgcoder2027@gmail.com>
2025-01-19 05:20:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,16 @@
+let score = 16
+console.log(typeof score)
+
+let valueinstring = String(score) // yahan par jo score ka datatype number mein tha woh ab string mein ho chuka hai
+
+console.log(typeof valueinstring)
+console.log(valueinstring)
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
