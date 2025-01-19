0c74652c6d0a1db168ed1870e218178a94d450bf
mgcoder27 <mgcoder2027@gmail.com>
2025-01-19 12:54:17 +0000

    datatypes summary done

---
+++ 01_basics/datatypessummary.js
@@ -0,0 +1,18 @@
+// Primitive
+/* 7 types : String, Number, Boolean, null, undefined, Symbol, bigint */ 
+
+// Reference(Non-Primitive)
+// Array, Objects, Functions
+
+const pokemon = ["Pikachu", "Charizard", "Bulbasaur"];
+
+let myobj = {
+    name: "Manu",
+    age: 20,
+}
+
+const myFunc = function(){
+    console.log("Hello World")
+}
+console.log(typeof pokemon)
+console.log(typeof myFunc)
