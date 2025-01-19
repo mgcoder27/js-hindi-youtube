9d935ecad056992226d391f83208da6540db3e98
mgcoder27 <mgcoder2027@gmail.com>
2025-01-19 06:05:11 +0000

    Operations done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,10 +1,12 @@
+
+// *******************Conversions****************
 let score = 16
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueinstring = String(score) // yahan par jo score ka datatype number mein tha woh ab string mein ho chuka hai
 
-console.log(typeof valueinstring)
-console.log(valueinstring)
+/*console.log(typeof valueinstring)
+console.log(valueinstring)*/
 
 // "33"=> 33
 // "33abc" => NaN
@@ -13,4 +15,33 @@ console.log(valueinstring)
 let isLoggedIn = 1
 
 let booleanLoggedIn = Boolean(isLoggedIn)
-console.log(booleanLoggedIn)
+// console.log(booleanLoggedIn)
+
+//  **************************** OPERATIONS ****************
+let val = 3
+let negvalue = -val
+// console.log(negvalue)
+
+let str1 = "Hello"
+let str2 = "Manu";
+
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3+4)*5%3)
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 16;
+gameCounter++;
+console.log(gameCounter);
