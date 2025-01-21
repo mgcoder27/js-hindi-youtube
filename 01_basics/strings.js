769cfac3301806b4dab76ac1e94ddaa1a4d6264d
mgcoder27 <mgcoder2027@gmail.com>
2025-01-21 13:20:52 +0000

    Strings Done

---
+++ 01_basics/strings.js
@@ -0,0 +1,19 @@
+const name = "Manu Garg";
+const age = 20;
+const clg = "MANIT Bhopal";
+
+console.log(`Hello My name is ${name} and I am ${age} years old and currently I am studying in ${clg}`);
+/* Above method is currently using for writing any thing with variable in Javascript*/
+
+const codename = new String('coderMG');
+
+console.log(codename[2]);
+console.log(codename.length);
+console.log(codename.toUpperCase());
+
+console.log(codename.charAt(4));
+console.log(codename.indexOf('e'));
+
+console.log(codename.substring(1,5));
+
+
