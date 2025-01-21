3050c2c5eae2119ae91143255af73abf3dae06cd
mgcoder27 <mgcoder2027@gmail.com>
2025-01-21 17:33:46 +0000

    Math and num done

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(200);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const othernum = 123.56782;
+console.log(othernum.toPrecision(5));
+
+const hundreds = 3400000000;
+console.log(hundreds.toLocaleString('en-IN')); // this will change the value according to Indian system commas
+
+
+//*****************MATHS**************** */
+
+console.log(Math);
+console.log(Math.abs(-35));
+console.log(Math.round(2.56));
+
+console.log(Math.min(4, 5 ,8, 2, -1));
+
+console.log(Math.max(3, 6, 9, -1, 4));
+
+console.log(Math.random()); // Most used library of math used in Web Development 
