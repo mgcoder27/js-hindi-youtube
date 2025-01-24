0ab285df761dca82e7e02ce99381aa204213322a
manugargMG1602 <manugarg1602@gmail.com>
2025-01-24 16:33:40 +0530

    Objects part 1 done

---
+++ 02_basics/objects.js
@@ -0,0 +1,24 @@
+// Objects
+
+// Object Literals
+const JSUser = {
+    name : "Manu",
+    age : 20,
+    location : "Bhopal",
+    email : "manugarg1602@gmail.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday","Saturday"]
+}
+
+console.log(JSUser.email);
+console.log(JSUser["name"]);
+
+JSUser.email = "mgcoder27@gmail.com" // Email has been changed
+console.log(JSUser.email);
+//Object.freeze(JSUser); // ab JSUser mein koi change nhi hoga
+
+JSUser.greeting = function(){
+    console.log(`Hello ${this.name}`);
+    
+}
+console.log(JSUser.greeting());
