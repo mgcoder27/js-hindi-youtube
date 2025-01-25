e34d284c82dd91d593f058f486fed199cca876e7
manugargMG1602 <manugarg1602@gmail.com>
2025-01-25 10:45:09 +0530

    Functions with arrays and objects done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -28,3 +28,36 @@ function loginUserMessage(username){
 }
 
 console.log(loginUserMessage()); // yahan par koi bhi argument pass nhi hua hai isliye yeh undefined answer dega lekin null nhi dega
+
+function calculateCarPrice(...num1){ // yahan par three dots ki wajah se yeh rest operator ban gya
+    return num1;
+}
+
+console.log(200,300,400);
+
+const user = {
+    username : "mgcoder27",
+    email : "mgarg2790@gmail.com"
+}
+
+function handleobject(anyobject){
+    console.log(`Username is ${anyobject.username} & his email is ${anyobject.email}`);
+    
+}
+//handleobject(user);
+
+// dusra tarika
+handleobject({
+    username:"mgcoder27",
+    email: "manugarg1602@gmail.com"
+})
+
+// Function in Arrays
+
+const arr = [100,300,500,700,900];
+
+function returnvalue(arr1){
+    return arr1[3];
+}
+
+console.log(returnvalue(arr));
